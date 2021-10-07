1cb83df6d6cc58d5ca1ab3a1ff980f7f5dc91f51
yukamyaw <yyyyyy325yyyyyy@yahoo.co.jp>
2021-10-07 11:13:47 +0900

    mysql導入したよ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get "posts/index" => "home#index"
+  get "top/index" => "top#index"
+  get "home/top" => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
